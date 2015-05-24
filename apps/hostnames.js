9bee8dbcda66cd2db389b568fd62db4ed84942f8
Thomas Pineau <tpineau.esc@gmail.com>
2015-05-24 14:50:30 +0200

    test

--- apps/hostnames.js
+++ apps/hostnames.js
@@ -2,6 +2,11 @@
 /**
  * Created by tpineau
  *
+ * query : {
+ *  db: 'string',              le nom de la mongoDB
+ *  col: 'string';             la collection contenant les hostnames
+ *  class: 'string'            la classification cible (all pour toute)
+ * }
  */
 
 var async = require('async');
@@ -20,6 +25,9 @@ function run(query, app){
         if (err){app.json({error: err});}
         else {
             var results = {};
+            query.target = {};
+            if (query.class != 'all' && query.class != 'null'){query.target.class = query.class;}
+            else if (query.class === 'null') {query.target.class = null;}
             async.each(
                 ['platforms', 'keywords', 'data'],
                 function (item, cb){
@@ -40,7 +48,7 @@ function run(query, app){
 }
 
 requests.data = function(db, query, callback){
-    db.collection(query.col).find({}).toArray(function (err, res) {
+    db.collection(query.col).find(query.target).toArray(function (err, res) {
         if (err) {callback(err);}
         else {callback(null, res);}
     });
@@ -51,9 +59,9 @@ requests.platforms = function(db, query, callback){
     async.each(
         database.platforms,
         function(item, cb) {
-            var target = {};
-            if (item != 'total') target = {platforms: item};
-            db.collection(query.col).count(target, function (err, res) {
+            //var target = {};
+            if (item != 'total') query.target.platforms = item;
+            db.collection(query.col).count(query.target, function (err, res) {
                 if (err) {results[item] = 'error';}
                 else {results[item]= res;}
                 cb();
@@ -71,9 +79,9 @@ requests.keywords = function(db, query, callback){
     async.each(
         database.keywords,
         function(item, cb) {
-            var target = {};
-            if (item != 'total') target = {keywords: item};
-            db.collection(query.col).count(target, function (err, res) {
+            //var target = {};
+            if (item != 'total') query.target.keywords = item;
+            db.collection(query.col).count(query.target, function (err, res) {
                 if (err) {results[item] = 'error';}
                 else {results[item]= res;}
                 cb();
