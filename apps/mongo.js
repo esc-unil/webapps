c6ae537f225f955c79acac37b8e21299a6f9da18
Thomas Pineau <tpineau.esc@gmail.com>
2015-05-24 09:27:32 +0200

    test

---
+++ apps/mongo.js
@@ -0,0 +1,38 @@
+'use strict';
+/**
+ * Created by tpineau
+ *
+ */
+
+var async = require('async');
+var mongoClient = require('mongodb').MongoClient;
+
+function run(database, col, target){
+//
+    var login = '';
+    if (monitoring.mongoDB.user != '' && monitoring.mongoDB.password != ''){
+        login = monitoring.mongoDB.user + ':' + monitoring.mongoDB.password;
+    }
+    var mongoPath = 'mongodb://' + login + monitoring.mongoDB.domain + ':' + monitoring.mongoDB.port + '/' + database;
+    mongoClient.connect(mongoPath, function(err, db) {
+        if (err){console.log(err);}
+        else {
+            var i = 1;
+            async.eachSeries(
+                platforms,
+                function (platform, cb){
+                    platform.getURL(db, col, target, function(err){
+                        if (err) {console.log(err);}
+                        console.log('Platform ' + i.toString() + '/' + platforms.length.toString());
+                        i++;
+                        cb();
+                    });
+                },
+                function (){
+                    db.close();
+                    console.log('done');
+                }
+            )
+        }
+    });
+}
--- server.js
+++ server.js
@@ -1,4 +1,34 @@
 'use strict';
 /**
  * Created by tpineau
+ *
+ * node --harmony server.js
  */
+
+var app = require('koa')();
+var router = require('koa-router')();
+var cors = require('koa-cors');
+
+var mongo = require('./apps/mongo.js');
+
+
+/*
+
+router.get('/websearch/:db/:param', function *(next) {
+ this.body = this.params;
+ console.log(this.params);
+});
+*/
+
+
+
+router.get('/websearch', function *(next) {
+ this.body = this.query;
+});
+
+
+app.name = 'Web Intelligence - UNIL';
+app.use(cors());
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen('3000');
